821066441eef79b3a2b54ba32623d8269221ec86
VulpineReynard <milare8@gmail.com>
2019-09-05 13:05:26 -0400

    environment set to run dynamic port

--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,4 @@
 module.exports = {
-  googleApiKey: process.env.GOOGLE_API_KEY || 'fake good api key'
+  googleApiKey: process.env.GOOGLE_API_KEY || 'fake good api key',
+  environment: process.env.NODE_ENV || 'development'
 };
--- data/db.js
+++ data/db.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
+const secrets = require('../config/secrets.js');
+
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 
